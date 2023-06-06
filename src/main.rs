616bd5efed24173cf710bed0bb916a54efac90be
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-06 15:52:19 +0530

    camera code added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("gst-launch-1.0")
+        .args(&[
+            "autovideosrc",
+            "!",
+            "queue",
+            "!",
+            "videoconvert",
+            "!",
+            "jpegenc",
+            "!",
+            "filesink",
+            "location=captured_image.jpg",
+        ])
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        println!("Image captured successfully!");
+    } else {
+        println!("Command failed with error: {:?}", output.status);
+        if let Ok(stderr) = String::from_utf8(output.stderr) {
+            println!("Error message: {}", stderr);
+        }
+    }
 }
