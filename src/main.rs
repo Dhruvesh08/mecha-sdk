2a5da0d8d4e01570226f70bb84efa86bfda3278c
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-06 16:50:54 +0530

    video capture added

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
 use std::process::Command;
 
 fn main() {
-    let args = [
-        "v4l2src",
-        "device=/dev/video0",
-        "!",
-        "video/x-raw, width=1280, height=720, framerate=30/1",
-        "vpuenc_h264",
-        "!",
-        "filesink",
-        "location=captured_video.h264",
-    ];
-
     let output = Command::new("gst-launch-1.0")
-        .args(&args)
+        .args(&[
+            "v4l2src",
+            "device=/dev/video0",
+            "!",
+            "video/x-raw, width=1280, height=720, framerate=30/1",
+            "!",
+            "vpuenc_h264",
+            "!",
+            "filesink",
+            "location=capture_video.h264",
+        ])
         .output()
-        .expect("Failed to execute command");
+        .expect("Failed to execute command.");
 
     if output.status.success() {
-        println!("Image captured successfully!");
+        println!("Command executed successfully.");
     } else {
-        println!("Command failed with error: {:?}", output.status);
-        if let Ok(stderr) = String::from_utf8(output.stderr) {
-            println!("Error message: {}", stderr);
-        }
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        println!("Command failed with error: {}", stderr);
     }
 }
