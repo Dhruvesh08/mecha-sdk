27942dfe78c22eb278632e2829b92e22a0eb0c28
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-06 16:38:35 +0530

    video capture added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::process::Command;
 
 fn main() {
+    let args = [
+        "v4l2src",
+        "device=/dev/video0",
+        "!",
+        "video/x-raw, width=1280, height=720, framerate=30/1",
+        "vpuenc_h264",
+        "!",
+        "filesink",
+        "location=captured_video.h264",
+    ];
+
     let output = Command::new("gst-launch-1.0")
-        .args(&[
-            "autovideosrc",
-            "!",
-            "queue",
-            "!",
-            "videoconvert",
-            "!",
-            "jpegenc",
-            "!",
-            "filesink",
-            "location=captured_image.jpg",
-        ])
+        .args(&args)
         .output()
         .expect("Failed to execute command");
 
