e77e30fb0230af603b3a292b0888c717feba1564
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-07 13:27:22 +0530

    image capture using gst

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,60 @@
-use std::process::Command;
+use gstreamer::prelude::*;
+use gstreamer::Element;
+use gstreamer::ElementFactory;
+use gstreamer::Message;
+use gstreamer::MessageView;
+use gstreamer::Pipeline;
+use std::error::Error;
 
-fn main() {
-    let output = Command::new("gst-launch-1.0")
-        .args(&[
-            "v4l2src",
-            "device=/dev/video0",
-            "!",
-            "video/x-raw, width=1280, height=720, framerate=30/1",
-            "!",
-            "vpuenc_h264",
-            "!",
-            "filesink",
-            "location=capture_video.h264",
-        ])
-        .output()
-        .expect("Failed to execute command.");
+fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize GStreamer
+    gstreamer::init()?;
 
-    if output.status.success() {
-        println!("Command executed successfully.");
-    } else {
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        println!("Command failed with error: {}", stderr);
+    // Create a pipeline
+    let pipeline = Pipeline::new(None);
+
+    // Create a v4l2src element
+    let src = ElementFactory::make("v4l2src").build().unwrap();
+
+    // Create a jpegenc element
+    let enc = ElementFactory::make("jpegenc").build().unwrap();
+
+    // Create a filesink element
+    let sink = ElementFactory::make("filesink").build().unwrap();
+
+
+
+    // Get an instance of the sink element
+    let sink = sink.clone().dynamic_cast::<Element>().unwrap();
+
+    // Set the output file name
+    sink.set_property("location", &"test_using_gst.jpg");
+
+    // Add elements to the pipeline
+    pipeline.add_many(&[&src, &enc, &sink]).unwrap();
+
+    // Link the elements together
+    src.link(&enc).unwrap();
+    enc.link(&sink).unwrap();
+
+    // Start the pipeline
+    pipeline.set_state(gstreamer::State::Playing)?;
+
+    // Wait for EOS or error message
+    let bus = pipeline.bus().unwrap();
+    for msg in bus.iter() {
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!("Error from {:?}:", err);
+                break;
+            }
+            _ => (),
+        }
     }
+
+    // Stop the pipeline
+    pipeline.set_state(gstreamer::State::Null)?;
+
+    Ok(())
 }
